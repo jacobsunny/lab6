c87527b9699e989802bc318dcec2eddf6862da30
jacob <tuf70239@temple.edu>
2018-03-13 19:18:21 -0400

    lab6 submission

---
+++ Documents/Visual Studio 2015/Projects/lab6/lab6/ticktacktoe.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTackToe
+{
+    public class tickTackToe
+    {
+
+
+
+
+
+
+
+
+        public enum Player
+        {
+
+            x,
+            o
+        }
+        Player currentPlayer = Player.x;
+        char[,] board = new char[3, 3];
+
+
+        // creating board 
+
+        public  int createBoard()
+        {
+            int[] board = new int[9];
+            int x = board.Length;
+            return x;
+        }
+
+
+
+        public char CurrentPlayer(int player1)
+        {
+            if (player1 == 0)
+            {
+                return 'x';
+            }
+            else
+                return 'o';
+        }
+
+        public string ChangePlayer(int player)
+        {
+            if (player == 1)
+            {
+                return "player1";
+            }
+            else
+                return "player2";
+        }
+
+        /*  void MakeMove()
+          {
+
+
+
+          }
+          */
+        /* bool SpaceInUSe()
+         {
+
+
+
+
+         }
+
+     */
+    }
+
+}
---
+++ Documents/Visual Studio 2015/Projects/lab6/lab6Tests/tickTackToeTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTackToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTackToe.Tests
+{
+    [TestClass()]
+    public class tickTackToeTests
+    {
+        [TestMethod()]
+        public void createBoardTest()
+        {
+            // Arrange
+            tickTackToe tick = new tickTackToe();
+
+            //Act
+            int x = tick.createBoard();
+
+            //Assert
+            Assert.AreEqual(9, x);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerTest()
+        {
+            // Arrange
+            tickTackToe tick = new tickTackToe();
+
+            //Act
+            char x = tick.CurrentPlayer(0);
+            char y = tick.CurrentPlayer(1);
+            //Assert
+            Assert.AreEqual('x', x);
+            Assert.AreEqual('o', y);
+        }
+
+        [TestMethod()]
+        public void ChangePlayerTest()
+        {
+            // Arrange
+            tickTackToe tick = new tickTackToe();
+
+            //Act
+            string x = tick.ChangePlayer(1);
+            string y = tick.ChangePlayer(0);
+            //Assert
+            Assert.AreEqual("player1", x);
+            Assert.AreEqual("player2", y);
+        }
+    }
+}
